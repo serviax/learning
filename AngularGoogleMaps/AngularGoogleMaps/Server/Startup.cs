d2aac1b4f66a5b9f4398f3398d807511139fe0c7
serviax <servi@telenet.be>
2015-09-14 21:47:02 +0200

    Added controller Added initial swashbuckle configuration

---
+++ AngularGoogleMaps/AngularGoogleMaps/Server/Startup.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+using WebApi.Hal;
+
+[assembly: OwinStartup(typeof(AngularGoogleMaps.Startup))]
+
+namespace AngularGoogleMaps
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+
+            var config = new HttpConfiguration();
+            config.Formatters.Add(new JsonHalMediaTypeFormatter());
+            config.Formatters.Add(new XmlHalMediaTypeFormatter());
+
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+              );
+
+            //config
+            //    .EnableSwagger(c => c.SingleApiVersion("v1", "My own api"))
+            //    .EnableSwaggerUi();
+
+            app.UseWebApi(config);
+
+            //app.Run(Configure);
+        }
+
+        //private void ConfigureSwagger(SwaggerDocsConfig configuration)
+        //{
+
+        //    configuration.SingleApiVersion("v1", "My API");
+        //    //configuration.IncludeXmlComments("bin\\AngularGoogleMaps.xml");
+        //}
+
+        ////private Task Configure(IOwinContext context)
+        ////{
+        ////    context.Response.ContentType = "plain/text"; ;
+        ////    return context.Response.WriteAsync("Hello world");
+        ////}
+    }
+}
---
+++ AngularGoogleMaps/AngularGoogleMaps/Server/TimeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Web.Http;
+
+namespace AngularGoogleMaps.Server
+{
+
+    /// <summary>
+    /// Time Controller
+    /// </summary>
+    [RoutePrefix("api/time")]
+    public class TimeController : ApiController
+    {
+        /// <summary>
+        /// gets the current time
+        /// </summary>
+        /// <returns>current time</returns>
+        [Route("get-time")]
+        public string GetCurrentTime()
+        {
+            return DateTime.Now.ToString();
+        }
+    }
+}
