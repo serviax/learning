d8745c0e4293bb41d4f988ccbcc23184e153b317
serviax <servi@telenet.be>
2016-05-18 16:00:51 +0200

    first version using visual code, typescript and angular 2.0

---
+++ FizzBuzzVisualCode/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: '<h1>My First Angular 2 App running at'+ new Date().toISOString() + '</h1>',
+})
+export class FizzBuzzCalculcator {
+    /**
+     *
+     */
+    constructor() {
+        super();
+        
+    }
+ }
